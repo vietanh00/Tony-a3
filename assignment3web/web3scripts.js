e8e57de1b3ad3c66afe8e4d061608d67620f3c15
vietanhrmit <s3928859@rmit.edu.vn>
2022-01-10 22:49:17 +0700

    change logo on light and dark mode

--- assignment3web/web3scripts.js
+++ assignment3web/web3scripts.js
@@ -2,11 +2,11 @@
 const light = ["MT4", "MT3", "MT2", "MT1","TnT", "Main_Part_Text2_1", "Main_Part_Text2",
         "Project_Text2", "Main_Part_Text", "Project_Text", "Project_Idea_Part2", "HardwareText2",
         "HardwareText", "Project_Des", "Project_Content", "Text1", "Header1","Project_Idea_Part","ToolandTech", 
-        "body_bg", "Box1_2", "Box1_1"];
+        "body_bg", "Box1_2", "Box1_1", "logo_light"];
     const dark = ["MT4dark", "MT3dark", "MT2dark","MT1dark", "TnTdark", "Main_Part_Text2_1dark", "Main_Part_Text2dark",
         "Project_Text2dark", "Main_Part_Textdark", "Project_Textdark", "Project_Idea_Part2dark", "HardwareText2dark",
         "HardwareTextdark", "Project_Desdark", "Project_Contentdark", "Text1dark", "Header1dark", "Project_Idea_Partdark",
-    "ToolandTechdark", "body_bgdark", "Box1_2dark", "Box1_1dark"];
+    "ToolandTechdark", "body_bgdark", "Box1_2dark", "Box1_1dark", "logo_dark"];
 function dark_mode(){
     for (var count = 0; count <dark.length; count++){
         var bg = document.getElementsByClassName(light[count]);
